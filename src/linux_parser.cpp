9163a6a1f2fa32db8e3e68da51cc0c37ebe37b17
Viktor <viktormihailovviksa@gmail.com>
2021-08-05 20:31:09 +0000

    Changes to the OS

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -53,11 +53,13 @@ string LinuxParser::OperatingSystem() {
   std::ifstream filestream(kOSPath);
   if (filestream.is_open()) {
     while (std::getline(filestream, line)) {
+      std::replace(line.begin(), line.end(), ' ', '_');
       std::replace(line.begin(), line.end(), '=', ' ');
       std::replace(line.begin(), line.end(),'"', ' ');
       std::istringstream linestream(line);
       while (linestream >> key >> value) {
         if (key == "PRETTY_NAME") {
+          std::replace(value.begin(), value.end(), '_', ' ');
           return value;
         }
       }
